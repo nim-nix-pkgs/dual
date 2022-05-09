eb8014ad55a841a4f40a5034c83af3db6dae83da
hugosenari <hugosenari@gmail.com>
2022-05-09 20:36:52 -0300

    chore: re index dual

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."dual-master".dir   = "master";
-  inputs."dual-master".owner = "nim-nix-pkgs";
-  inputs."dual-master".ref   = "master";
-  inputs."dual-master".repo  = "dual";
-  inputs."dual-master".type  = "github";
-  inputs."dual-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."dual-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
